863e341d06e001f1cfc64dc7c22a4d44a77b82ed
icicala <icicala.ubb@gmail.com>
2021-12-02 18:19:48 +0100

    feat(MAD 3 APPS UIs) Simple adapter

--- app/src/main/java/de/thu/hallomad/MainActivity.java
+++ app/src/main/java/de/thu/hallomad/MainActivity.java
@@ -3,6 +3,8 @@ package de.thu.hallomad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_view_item, R.id.list_item_text, months);
+
+        ListView listView = findViewById(R.id.list_view_id);
+        listView.setAdapter(adapter);
     }
 }
