2bc893a5d12d80af389b941e53882508aa37c363
icicala <icicala.ubb@gmail.com>
2021-12-03 14:53:25 +0100

    feat(Exercises 2) Exercise 2.2: Currency Converter (List of Currencies)

---
+++ app/src/main/java/de/thu/hallomad/ExchangeRate.java
@@ -0,0 +1,20 @@
+package de.thu.hallomad;
+
+public class ExchangeRate {
+
+    private String currencyName;
+    private double rateForOneEuro;
+
+    public ExchangeRate(String currencyName, double rateForOneEuro) {
+        this.currencyName = currencyName;
+        this.rateForOneEuro = rateForOneEuro;
+    }
+
+    public String getCurrencyName() {
+        return currencyName;
+    }
+
+    public double getRateForOneEuro() {
+        return rateForOneEuro;
+    }
+}
--- app/src/main/java/de/thu/hallomad/MainActivity.java
+++ app/src/main/java/de/thu/hallomad/MainActivity.java
@@ -3,6 +3,8 @@ package de.thu.hallomad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExchangeRateDatabase data = new ExchangeRateDatabase();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_view, R.id.id_spinner_item, data.getCurrencies());
+
+        Spinner spinnerFrom = findViewById(R.id.id_spinner_from);
+        spinnerFrom.setAdapter(adapter);
+
+        Spinner spinnerTo = findViewById(R.id.id_spinner_to);
+        spinnerTo.setAdapter(adapter);
     }
 }
