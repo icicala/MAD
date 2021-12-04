25c7f931c1bca178001a888c6325a84dcd13adfe
icicala <icicala.ubb@gmail.com>
2021-12-04 21:11:46 +0100

    feat(Week 4 onTouch) Reacting on UI Events

--- app/src/main/java/de/thu/hallomad/MainActivity.java
+++ app/src/main/java/de/thu/hallomad/MainActivity.java
@@ -3,6 +3,9 @@ package de.thu.hallomad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView smiley = findViewById(R.id.id_smiley_view);
+        View container = findViewById(R.id.id_activity_main);
+
+        container.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                if (action == MotionEvent.ACTION_MOVE || action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN) {
+                    float x = event.getX();
+                    float y = event.getY();
+
+                    smiley.setX(x - smiley.getWidth() / 2);
+                    smiley.setY(y - smiley.getHeight() / 2);
+                }
+                return true;
+            }
+        });
+
     }
 }
