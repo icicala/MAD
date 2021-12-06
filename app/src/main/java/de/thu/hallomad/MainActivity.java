4d3ea8858273a6cf3995301b499ec98dd8e80f5c
icicala <icicala.ubb@gmail.com>
2021-12-06 20:11:25 +0100

    feat(Week 5) Offrer Activities to others

--- app/src/main/java/de/thu/hallomad/MainActivity.java
+++ app/src/main/java/de/thu/hallomad/MainActivity.java
@@ -2,7 +2,9 @@ package de.thu.hallomad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        if (intent.getAction().equals(Intent.ACTION_SEND)) {
+            Log.i("Share", intent.getStringExtra(Intent.EXTRA_TEXT));
+        }
+
+
     }
 }
