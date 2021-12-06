82e4af800d351006094c600f3fd81f7f5b06a37a
icicala <icicala.ubb@gmail.com>
2021-12-06 17:37:31 +0100

    feat(Week 5) Implicit Intents - Using external Activities

--- app/src/main/java/de/thu/hallomad/MainActivity.java
+++ app/src/main/java/de/thu/hallomad/MainActivity.java
@@ -1,8 +1,12 @@
 package de.thu.hallomad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.IntentCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View view) {
+        // View web page
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://thu.de"));
+        // open app of category
+//        Intent intent = IntentCompat.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_CALENDAR);
+        // open dialing app
+//        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+49/731-5028533"));
+
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+49 733 455555555"));
+        startActivity(intent);
     }
 }
