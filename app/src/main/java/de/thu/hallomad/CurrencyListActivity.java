f7869cba6c331cbdf02fc8c5a4aeb1adccf527ff
icicala <icicala.ubb@gmail.com>
2021-12-07 14:24:47 +0100

    feat(Exercises 4) Exercises 4.2: Show Capital on Map

--- app/src/main/java/de/thu/hallomad/CurrencyListActivity.java
+++ app/src/main/java/de/thu/hallomad/CurrencyListActivity.java
@@ -2,13 +2,17 @@ package de.thu.hallomad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class CurrencyListActivity extends AppCompatActivity {
     private ExchangeRateDatabase data;
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +21,22 @@ public class CurrencyListActivity extends AppCompatActivity {
         data = new ExchangeRateDatabase();
         CurrencyItemAdapter adapter = new CurrencyItemAdapter(data);
 
-        ListView listView = findViewById(R.id.id_currency_list_view);
+        listView = findViewById(R.id.id_currency_list_view);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String currencyName = (String) adapter.getItem(position);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0`?q=" + data.getCapital(currencyName)));
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
+
 }
