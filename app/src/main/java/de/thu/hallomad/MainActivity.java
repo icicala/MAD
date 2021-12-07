549d79df60498b8aa07c460a9f8f6457e6e5d4b8
icicala <icicala.ubb@gmail.com>
2021-12-07 13:25:08 +0100

    feat(Exercises 4) Exercises 4.1: Add a menu to Start a new Activity

--- app/src/main/java/de/thu/hallomad/MainActivity.java
+++ app/src/main/java/de/thu/hallomad/MainActivity.java
@@ -1,13 +1,20 @@
 package de.thu.hallomad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
@@ -23,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Toolbar initializing
+        Toolbar toolbar = findViewById(R.id.id_toolbar);
+        setSupportActionBar(toolbar);
 
         data = new ExchangeRateDatabase();
         CurrencyItemAdapter adapter = new CurrencyItemAdapter(data);
@@ -54,5 +65,27 @@ public class MainActivity extends AppCompatActivity {
         decimalFormat.setRoundingMode(RoundingMode.CEILING);
         double finalResult = data.convert(valueInserted, source, target);
         resultView.setText(decimalFormat.format(finalResult));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_currency_list_menu:
+                Intent intent = new Intent(MainActivity.this, CurrencyListActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
     }
 }
