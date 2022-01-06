c3ae1c1f20516857f0bcf22e971153005a5f0c90
icicala <icicala.ubb@gmail.com>
2022-01-06 12:26:41 +0100

    feat(Week 10) MediaPlayer relaxing app

--- app/src/main/java/de/thu/hallomad/MainActivity.java
+++ app/src/main/java/de/thu/hallomad/MainActivity.java
@@ -2,13 +2,27 @@ package de.thu.hallomad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player = MediaPlayer.create(this, R.raw.luftrum__forestsurroundings);
+        player.setLooping(true);
+    }
+
+    public void imageClicked(View view) {
+        if (!player.isPlaying()) {
+            player.start();
+        } else {
+            player.pause();
+        }
     }
 }
